c2753e926e320a8548f329f52afe8dfecdc1b9d4
milkchocolatejr <alexanderwilliams29@gmail.com>
2025-06-19 11:04:23 -0500

    added command line interface. now we can start to cook, stable

--- brain.c
+++ brain.c
@@ -1,10 +1,39 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(int argc, char *argv[]){
-    printf("Hello World!");
-    return 0;
+void help(){
+    printf("You need help!\n");
 }
 
-void validate(char* path){
+void validate(char* myPath){
+    printf("You would like to validate %s, right?\n", myPath);
+}
+
+int main(int argc, char *argv[]){
+    printf("Welcome to the brains of the operation!\n");
     
+    if(argc < 2){
+        printf("You didn't tell me what to do :c");
+        return 0;
+    }
+
+    //Command Parsing
+
+    char* command = argv[1];
+    if(strcmp(command, "help") == 0){ //Two strings are identifcal
+        if(argc != 2){
+            printf("For help, please use: './brain.o help'.\n");
+            return 0;
+        }
+        help();
+    }
+    else if(strcmp(command, "validate") == 0){
+        if(argc != 4){
+            printf("To validate, please use: './brain.o validate -p <path>\n");
+            return 0;
+        }
+        validate(argv[3]);
+    }
+
+    return 0;
 }
--- makefile
+++ makefile
@@ -1,3 +1,19 @@
-run:	
+runbasic:	
 	gcc brain.c -o brain.o
 	./brain.o
+help: 
+	gcc brain.c -o brain.o
+	./brain.o help
+
+helpTest:
+	gcc brain.c -o brain.o
+	./brain.o help please!
+
+validate:
+	gcc brain.c -o brain.o
+	./brain.o validate -p myPath.txt
+
+validateTest:
+	gcc brain.c -o brain.o
+	./brain.o validate
+
